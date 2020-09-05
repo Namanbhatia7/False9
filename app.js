75eb6c0b8ab732d8af4894183f79ee7c3f3eeff5
Naman Bhatia <bhatia.naman@hotmail.com>
2020-09-05 20:20:01 +0530

    css

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
 
 app.get("/", (req,res) => {
     res.sendFile(`${__dirname}/index.html`);
