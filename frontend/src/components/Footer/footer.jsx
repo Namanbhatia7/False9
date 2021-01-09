17f6c37fe1d3cafe0e3864aa2b81412baf8e2e7e
Nikhil Sharma <nikhilsharma230303@gmail.com>
2021-01-10 03:54:17 +0530

    NikhilSharmaWe fixed #26

--- frontend/src/components/Footer/footer.jsx
+++ frontend/src/components/Footer/footer.jsx
@@ -10,7 +10,10 @@ function Footer() {
           Made using{" "}
           <a href="https://www.football-data.org/">Football-Data.org's API</a>
         </h6>
-        <h6 className="footer-text">&#169; {date}</h6>
+        <h6 className="footer-text">Dynamic Copyright</h6>
+        <h6 className="footer-text">Made with help of
+        <a href="football-data.org"> football-data.org</a></h6>
+      <h6 className="footer-text">&#169; {date}</h6>
       </div>
     </footer>
   );
