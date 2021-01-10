c7ef1481aa0ebc31f73f58a1b7ed63b5ea183eb6
Naman Bhatia <bhatia.naman@hotmail.com>
2021-01-11 01:24:07 +0530

    added index.js

---
+++ public/js/index.js
@@ -0,0 +1,8 @@
+function toggleSquad(){
+    let x = document.getElementById("toggle");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+}
